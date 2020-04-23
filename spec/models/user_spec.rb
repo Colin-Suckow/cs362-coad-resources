6647790ea374af247e1e3e495db0405da50ce441
Colin Suckow <csuckow99@gmail.com>
2020-04-23 13:24:50 -0700

    Cover users model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  let(:user) { User.new(email: "test_email@suckow.dev") }
+
   describe "attributes" do
     it { should respond_to :email }
     it { should respond_to :encrypted_password }
@@ -22,5 +24,31 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of :password }
     it { should validate_length_of(:password).is_at_least(7).is_at_most(255) }
+  end
+
+  describe "#set_default_role" do
+    it "sets role to :organization" do
+      user = User.new
+      user.role = nil
+      expect { user.set_default_role }.to change { user.role }.to "organization"
+    end
+
+    it "does not change existing role" do
+      user = User.new
+      user.role = :admin
+      expect { user.set_default_role }.not_to change { user.role }
+    end
+  end
+
+  describe "#to_s" do
+    describe "#to_s" do
+      it "returns a string" do
+        expect(user.to_s).to be_instance_of String
+      end
+
+      it "returns its email" do
+        expect(user.to_s).to eq "test_email@suckow.dev"
+      end
+    end
   end
 end
