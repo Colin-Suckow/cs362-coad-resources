a3ae0348bc642af9b24e0f8f8b04c6a20c18ac81
MaxTheMitchell <rat2043@gmail.com>
2020-04-23 12:46:45 -0700

    add attriube tests for all non index attriubes

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,7 +3,17 @@ require 'rails_helper'
 RSpec.describe Ticket, type: :model do
 
   describe "attributes" do 
-    # todo
+    it { should respond_to :name }
+    it { should respond_to :description }
+    it { should respond_to :phone }
+    it { should respond_to :organization_id}
+    it { should respond_to :closed }
+    it { should respond_to :closed_at }
+    it { should respond_to :resource_category_id }
+    it { should respond_to :region_id }
+#     it { should respond_to :index_tickets_on_organization_id }
+#     it { should respond_to :index_tickets_on_region_id }
+#     it { should respond_to :index_tickets_on_resource_category_id } 
   end
 
   describe "associations" do 
