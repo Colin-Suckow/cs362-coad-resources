dae362cbb1873b5b967b6ef64df61173b5a32fa6
Colin Suckow <csuckow99@gmail.com>
2020-04-23 13:28:03 -0700

    Format region_spec.rb

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 RSpec.describe Region, type: :model do
   let(:region) { Region.new(name: "FAKE") }
 
-  describe "attributes" do 
+  describe "attributes" do
     it { should respond_to :name }
   end
 
-  describe "associations" do 
-    it { should have_many :tickets}
+  describe "associations" do
+    it { should have_many :tickets }
   end
 
-  describe "validations" do 
+  describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
     it { should validate_uniqueness_of(:name).case_insensitive }
