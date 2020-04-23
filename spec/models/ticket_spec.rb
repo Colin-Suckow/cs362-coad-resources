d949f72a83a77c5c89022c2f8f29168e5d7fff3d
MaxTheMitchell <rat2043@gmail.com>
2020-04-23 13:26:29 -0700

    create all validation tests but one

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -23,7 +23,13 @@ RSpec.describe Ticket, type: :model do
   end
 
   describe "validations" do 
-    # todo
+    it { should validate_presence_of :name }    
+    it { should validate_presence_of :phone }
+    it { should validate_presence_of :region_id }
+    it { should validate_presence_of :resource_category_id }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+    it { should validate_length_of(:description).is_at_most(1020).on(:create) }
+    # it { should validate_presence_of(:phone).phony_plausible }
   end
 
   describe "scope" do
