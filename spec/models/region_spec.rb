fdb0717275258a5f0a77a1e165d45e885614a980
Colin Suckow <csuckow99@gmail.com>
2020-04-23 08:52:12 -0700

    Initial region tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+    it "has a name" do
+        expect(Region).to respond_to(:name)
+    end
+
+    specify {is_expected.to respond_to(:created_at)}
+    specify {is_expected.to respond_to(:updated_at)}
+
 end
