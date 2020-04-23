7037795d1bc50149a05b142c2599d3e329159135
MaxTheMitchell <rat2043@gmail.com>
2020-04-23 12:35:39 -0700

    stub out ticket spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  describe "attributes" do 
+    # todo
+  end
+
+  describe "associations" do 
+    #todo
+  end
+
+  describe "validations" do 
+    # todo
+  end
+
+  describe "scope" do
+    # todo
+  end
+
+  describe "#open?" do
+    #todo
+  end
+
+  describe "#captured?" do
+    # todo
+  end
+
+  describe "#to_s" do 
+    # tod
+  end
+
 end
