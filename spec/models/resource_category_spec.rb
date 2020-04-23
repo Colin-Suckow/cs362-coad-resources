e07c9cef9c11c10ef11ade8e1fac3300ed7a4548
Colin Suckow <csuckow99@gmail.com>
2020-04-23 08:58:54 -0700

    Add validation tests to ResourceCategory

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+    specify {is_expected.to respond_to(:name)}
+    specify {is_expected.to respond_to(:created_at)}
+    specify {is_expected.to respond_to(:updated_at)}
+    specify {is_expected.to respond_to(:active)}
+
 end
