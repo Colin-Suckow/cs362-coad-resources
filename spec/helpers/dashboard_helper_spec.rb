a4354f7c7870d5f8ba6c9bd6213af33cfd317eb1
MaxTheMitchell <rat2043@gmail.com>
2020-05-18 12:53:16 -0700

    write tests for dashboard_helper

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 # Specs in this file have access to a helper object that includes
 # the DashboardHelper. For example:
@@ -11,5 +11,32 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe DashboardHelper, type: :helper do
+  let(:user) do
+    user = double
+    user.stub(:admin?).and_return(false)
+    user.stub_chain(:organization, :submitted?).and_return(false)
+    user.stub_chain(:organization, :approved?).and_return(false)
+    user
+  end
 
+  describe "dashboard_for" do
+    it "returns 'admin_dashboard' if admin? returns true" do
+      user.stub(:admin?).and_return(true)
+      expect(dashboard_for(user)).to eq("admin_dashboard")
+    end
+
+    it "returns 'organization_submitted_dashboard' if user isn't an admin and its organization is submitted" do
+      user.stub_chain(:organization, :submitted?).and_return(true)
+      expect(dashboard_for(user)).to eq("organization_submitted_dashboard")
+    end
+
+    it "returns 'organization_approved_dashboard' if user isn't an admin and its organization is approved" do
+      user.stub_chain(:organization, :approved?).and_return(true)
+      expect(dashboard_for(user)).to eq("organization_approved_dashboard")
+    end
+
+    it "returns 'create_application_dashboard' if user isn't an admin and its organization isn't submitted or approved" do
+      expect(dashboard_for(user)).to eq("create_application_dashboard")
+    end
+  end
 end
