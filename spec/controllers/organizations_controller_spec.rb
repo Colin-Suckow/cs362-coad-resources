ab2d0e4c1f479ed906186a8a3fdd8454868061b6
MaxTheMitchell <rat2043@gmail.com>
2020-05-18 11:59:15 -0700

    write tests for organization user for organization controller spec

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -31,4 +31,39 @@ RSpec.describe OrganizationsController, type: :controller do
     end
   end
 
+  context "as an organization" do
+    let (:organization_user) { create(:user,role: "organization") }
+    let (:admin_user) {create(:user,role: "admin")}
+
+    before(:each) do
+      organization_user.confirm
+      admin_user.confirm
+      sign_in(organization_user)
+    end
+
+    describe "#index" do
+      specify { expect(get(:index)).to be_successful }
+    end
+
+    describe "#new" do
+      specify { expect(get(:new)).to be_successful }
+    end
+
+    describe "#create" do
+      specify { expect(get(:create,params: { organization: attributes_for(:organization) })).to redirect_to(organization_application_submitted_path) }
+    end
+
+    describe "#update" do
+      specify { expect(get(:update, params: { id: organization.id })).to redirect_to(dashboard_path) }
+    end
+
+    describe "#approve" do
+      specify { expect(get(:approve, params: { id: organization.id })).to redirect_to(dashboard_path) }
+    end
+
+    describe "#reject" do
+      specify { expect(get(:approve, params: { id: organization.id })).to redirect_to(dashboard_path) }
+    end
+  end
+
 end
