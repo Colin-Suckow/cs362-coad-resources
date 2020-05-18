320649933f883a1fe2f36286e6cc18b22906c9d3
MaxTheMitchell <rat2043@gmail.com>
2020-05-18 12:04:06 -0700

    write tests for admin user for organization controller spec

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -66,4 +66,37 @@ RSpec.describe OrganizationsController, type: :controller do
     end
   end
 
+  context "as an organization" do
+    let (:admin_user) {create(:user,role: "admin")}
+
+    before(:each) do
+      admin_user.confirm
+      sign_in(admin_user)
+    end
+
+    describe "#index" do
+      specify { expect(get(:index)).to be_successful }
+    end
+
+    describe "#new" do
+      specify { expect(get(:new)).to_not be_successful }
+    end
+
+    describe "#create" do
+      specify { expect(get(:create,params: { organization: attributes_for(:organization) })).to redirect_to(dashboard_path) }
+    end
+
+    describe "#update" do
+      specify { expect(get(:update, params: { id: organization.id })).to redirect_to(dashboard_path) }
+    end
+
+    describe "#approve" do
+      specify { expect(get(:approve, params: { id: organization.id })).to redirect_to(organizations_path) }
+    end
+
+    describe "#reject" do
+      specify { expect(get(:approve, params: { id: organization.id })).to redirect_to(organizations_path) }
+    end
+  end
+
 end
