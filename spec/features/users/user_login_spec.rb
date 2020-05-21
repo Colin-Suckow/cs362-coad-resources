b1e7e74746eea5703b7d94ca9af36bfef4cf2a0e
MaxTheMitchell <rat2043@gmail.com>
2020-05-21 09:50:50 -0700

    add the rest of organization user tests to user login spec

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,20 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Logging in', type: :feature do
+RSpec.describe "Logging in", type: :feature do
+  context "as an organization user" do
+    let(:organization_user) { create(:user, role: "organization") }
+    context "that is confirmed" do
+      before (:each) { organization_user.confirm }
 
-    context "as an organization user" do
-        let(:organization_user){create(:user, email:"FAKE@fake.com",password: "123456789",role: "organization")}
+      it "will display the organization dashbord when the user sucessuflly logs in" do
+        visit login_path
+        fill_in "user_email", with: organization_user.email
+        fill_in "user_password", with: organization_user.password
+        click_on "commit"
+        expect(page).to have_content("Dashboard")
+      end
 
-        before (:each) {organization_user.confirm}
-
-        it "user sucessuflly logs in and sees organization dashbord" do 
-            visit login_path
-            fill_in "user_email", with: organization_user.email
-            fill_in "user_password", with: organization_user.password 
-            click_on "commit"
-            expect(page).to have_content("Dashboard")
-        end
-
+      it "will tell the user that the login is invalid when the user logs in with an invalid value" do
+        visit login_path
+        fill_in "user_email", with: organization_user.email
+        fill_in "user_password", with: "invalid password"
+        click_on "commit"
+        expect(page).to have_content("Invalid")
+      end
     end
-
+    context "that is unconfired" do
+      it "will tell the user they aren't confirmed" do
+        visit login_path
+        fill_in "user_email", with: organization_user.email
+        fill_in "user_password", with: organization_user.password
+        click_on "commit"
+        expect(page).to have_content("confirm")
+      end
+    end
+  end
 end
