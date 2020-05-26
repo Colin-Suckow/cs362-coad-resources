1477b7bc2357c4a3a01d29aa8d0d33b1bb1b495c
MaxTheMitchell <rat2043@gmail.com>
2020-05-26 13:42:24 -0700

    write reject organization tests

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Rejecting an organization', type: :feature do
+RSpec.describe "Rejecting an organization", type: :feature do
+  context "as an admin" do
+    let(:organization) { create(:organization) }
 
+    before(:each) do
+      organization
+      admin = create(:user, role: "admin")
+      admin.confirm
+      log_in_as(admin)
+    end
+
+    it "displays a message indicating that the organization has been rejected" do
+      visit organizations_path
+      click_on organization.name
+      click_on "Reject"
+      expect(page).to have_content("#{organization.name} has been rejected")
+    end
+  end
 end
