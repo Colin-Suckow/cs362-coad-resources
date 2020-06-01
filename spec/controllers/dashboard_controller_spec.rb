89fbf646045422336cae238203248bac165ea8cf
Colin Suckow <csuckow99@gmail.com>
2020-06-01 13:41:04 -0700

    Add dashboard_controller test

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,38 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe DashboardController, type: :controller do
+  require "rails_helper"
 
+  context "as an unsigned user" do
+    let (:unsigned_in_user) { create(:user) }
+    describe "#index" do
+      specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+    end
+  end
+
+  context "as an organization" do
+    let (:organization_user) { create(:user, role: "organization") }
+
+    before(:each) do
+      organization_user.confirm
+      sign_in(organization_user)
+    end
+
+    describe "#index" do
+      specify { expect(get(:index)).to be_successful }
+    end
+  end
+
+  context "as an admin" do
+    let (:admin_user) { create(:user, role: "admin") }
+    let (:region) { create(:region) }
+    before(:each) do
+      admin_user.confirm
+      sign_in(admin_user)
+    end
+
+    describe "#index" do
+      specify { expect(get(:index)).to be_successful }
+    end
+  end
 end
