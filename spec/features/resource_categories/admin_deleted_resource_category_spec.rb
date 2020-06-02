c644e02e46ae451bb9b97b9e5eb0d031f91120c2
Colin Suckow <csuckow99@gmail.com>
2020-06-01 19:25:11 -0700

    Implement feature test for deleting resource categories

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Deleting a Resource Category', type: :feature do
+RSpec.describe "Deleting a Resource Category", type: :feature do
+  context "as an admin" do
+    let(:resource_category) { create(:resource_category) }
 
+    before(:each) do
+      resource_category
+      admin = create(:user, role: "admin")
+      admin.confirm
+      log_in_as(admin)
+    end
+
+    it "will tell the user that the category was deleted" do
+      visit resource_categories_path
+      click_on resource_category.name
+      click_on "Delete"
+      expect(page).to have_content("#{resource_category.name} was deleted")
+    end
+  end
 end
