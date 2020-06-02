5324115ec71c985d6c5130147e2e18d026891324
Colin Suckow <csuckow99@gmail.com>
2020-06-01 19:33:38 -0700

    Add admin deletes ticket feature test

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Deleting a Ticket', type: :feature do
+RSpec.describe "Deleting a Ticket", type: :feature do
+  context "as an admin" do
+    let(:ticket) { create(:ticket) }
 
+    before(:each) do
+      ticket
+      admin = create(:user, role: "admin")
+      admin.confirm
+      log_in_as(admin)
+    end
+
+    it "will tell the user that the ticket was deleted" do
+      visit dashboard_path
+      click_on ticket.name
+      click_on "Delete"
+      expect(page).to have_content("Ticket #{ticket.id} was deleted")
+    end
+  end
 end
